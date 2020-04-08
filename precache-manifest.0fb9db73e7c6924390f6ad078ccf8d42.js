191e0b5e0233a2208ffadd377af25ed0c0ab0806
Dan Popa <danpopa1998@gmail.com>
2020-04-08 19:55:59 +0200

    Updates

---
+++ precache-manifest.0fb9db73e7c6924390f6ad078ccf8d42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066629c4eba973e0af3787089d9faca1",
+    "url": "/custom-swedish-board/index.html"
+  },
+  {
+    "revision": "1999daa66e84737774b1",
+    "url": "/custom-swedish-board/static/css/main.ef4d000e.chunk.css"
+  },
+  {
+    "revision": "e5ef4eaa930ef9fe0ded",
+    "url": "/custom-swedish-board/static/js/2.b412ffef.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/custom-swedish-board/static/js/2.b412ffef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1999daa66e84737774b1",
+    "url": "/custom-swedish-board/static/js/main.2b146159.chunk.js"
+  },
+  {
+    "revision": "e9b344113d18c92ea5d1",
+    "url": "/custom-swedish-board/static/js/runtime-main.1a70e1b7.js"
+  }
+]);
